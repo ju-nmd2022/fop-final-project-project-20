933210fba84ebb5c599224da35ce170e678a212b
Emilia Fredriksson <frem22pu@student.ju.se>
2023-04-17 13:14:02 +0200

    defining html elements in javascript

---
+++ mouseChase.js
@@ -0,0 +1,14 @@
+// Buttons for starting and restarting game
+const startButton = document.getElementById("startButton");
+const restartButton = document.getElementById("restartButton");
+
+// Characters in the game
+const catSitting = document.getElementById("catSittting");
+const catStanding = document.getElementById("catStanding");
+const catLaying = document.getElementById("catLying");
+const mouse = document.getElementById("mouse");
+
+// Other elements in the game
+const mousetrap = document.getElementById("mousetrap");
+const magicPotion = document.detElementById("magicPotion");
+const cheeseBite = document.getElementById("cheeseBite");
